546ab7bd5f4a0c3095c8c7ee5cad455e0b90a56a
iisyos <kosuke1216a@yahoo.co.jp>
2025-03-02 00:24:43 +0900

    *chore: add vpc

---
+++ terraform/variable.tf
@@ -0,0 +1,5 @@
+variable "app_name" {
+  default     = "glue_poc"
+  description = "The name of the application"
+  type        = string
+}
---
+++ terraform/vpc.tf
@@ -0,0 +1,25 @@
+# @see https://registry.terraform.io/modules/kasaikou/vpc/aws/latest
+module "vpc" {
+  source     = "kasaikou/vpc/aws"
+  name       = var.app_name
+  cidr_block = "10.0.0.0/16"
+  subnets = {
+    "public-primary" = {
+      cidr_block        = "10.0.32.0/20"
+      availability_zone = "ap-northeast-1a"
+      route_tables      = ["igw"]
+    }
+    "private-primary" = {
+      cidr_block        = "10.0.64.0/20"
+      availability_zone = "ap-northeast-1a"
+    }
+  }
+  route_tables = {
+    "igw" = {
+      global_type = "igw"
+      vpc_gateway_endpoints = [
+        "com.amazonaws.ap-northeast-1.s3"
+      ]
+    }
+  }
+}
